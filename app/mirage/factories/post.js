03153bc02fbbaa08319cc9817ae3586266efebe9
Joe Heth <joeheth@gmail.com>
2015-09-22 18:35:34 -0400

    Adds Ember-CLI Mirage.

---
+++ app/mirage/factories/post.js
@@ -0,0 +1,25 @@
+/*
+  This is an example factory definition.
+
+  Create more files in this directory to define additional factories.
+*/
+import Mirage, {faker} from 'ember-cli-mirage';
+
+export default Mirage.Factory.extend({
+  title: faker.hacker.noun,
+  body: faker.lorem.sentences(10),
+  author: faker.name.findName(),
+  createdAt: faker.date.recent
+
+  // name: 'Pete',                         // strings
+  // age: 20,                              // numbers
+  // tall: true,                           // booleans
+
+  // email: function(i) {                  // and functions
+  //   return 'person' + i + '@test.com';
+  // },
+
+  // firstName: faker.name.firstName,       // using faker
+  // lastName: faker.name.firstName,
+  // zipCode: faker.address.zipCode
+});
