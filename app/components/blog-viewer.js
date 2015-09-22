f6f8cb8855895d680abd39f784188b977e37f32b
Joe Heth <joeheth@gmail.com>
2015-09-22 19:38:46 -0400

    Introduce button actions and state management.

--- app/components/blog-viewer.js
+++ app/components/blog-viewer.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['mdl-card', 'mdl-shadow--4dp', 'mdl-cell', 'mdl-cell--12-col']
+  classNames: ['mdl-card', 'mdl-shadow--4dp', 'mdl-cell', 'mdl-cell--12-col'],
+  isEditing: false,
+
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+    save: function() {
+      this.set('isEditing', false);
+    }
+  }
 });
